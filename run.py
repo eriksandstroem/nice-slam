623241c6647bdd0355c54f9e98232c3b53fe9ff2
tfy14esa <erik.sandstrm@gmail.com>
2022-06-08 15:35:12 +0200

    Test commit from nudel vscode

--- run.py
+++ run.py
@@ -15,30 +15,44 @@ def setup_seed(seed):
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
 
+
 def main():
-    # setup_seed(20)
+    setup_seed(20)
 
     parser = argparse.ArgumentParser(
-        description='Arguments for running the NICE-SLAM/iMAP*.'
+        description="Arguments for running the NICE-SLAM/iMAP*."
     )
-    parser.add_argument('config', type=str, help='Path to config file.')
-    parser.add_argument('--input_folder', type=str,
-                        help='input folder, this have higher priority, can overwrite the one in config file')
-    parser.add_argument('--output', type=str,
-                        help='output folder, this have higher priority, can overwrite the one in config file')
+    parser.add_argument(
+        "config", type=str, help="Path to config file.",
+    )
+    parser.add_argument(
+        "--input_folder",
+        type=str,
+        help="input folder, this have higher priority, can overwrite the one in config file",
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        help="output folder, this have higher priority, can overwrite the one in config file",
+    )
     nice_parser = parser.add_mutually_exclusive_group(required=False)
-    nice_parser.add_argument('--nice', dest='nice', action='store_true')
-    nice_parser.add_argument('--imap', dest='nice', action='store_false')
+    nice_parser.add_argument(
+        "--nice", dest="nice", action="store_true",
+    )
+    nice_parser.add_argument(
+        "--imap", dest="nice", action="store_false",
+    )
     parser.set_defaults(nice=True)
     args = parser.parse_args()
 
     cfg = config.load_config(
-        args.config, 'configs/nice_slam.yaml' if args.nice else 'configs/imap.yaml')
+        args.config, "configs/nice_slam.yaml" if args.nice else "configs/imap.yaml",
+    )
 
     slam = NICE_SLAM(cfg, args)
 
     slam.run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
